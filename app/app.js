d4a9c008cc833e0087595c5ccc8a6fc5a0a4af85
uyouthe <melvoloskov@ya.ru>
2020-08-30 18:18:53 +0300

    done

---
+++ app/app.js
@@ -0,0 +1,55 @@
+const actions = [
+    `делегируй`,
+    `декомпозируй`,
+    `проецируй`,
+    `распределяй`,
+    `кешируй`,
+    `консолидируй`,
+    `объединяй`,
+    `интерпретируй`,
+    `анализируй`,
+    `рационализируй`,
+    `геймифицируй`,
+    `фильтруй`,
+    `ограничивай`,
+    `изобретай`,
+    `централизируй`,
+    `коллекционируй`,
+]
+
+const subjects = [
+    `абстракции`,
+    `методы`,
+    `ритуалы`,
+    `сущности`,
+    `привычки`,
+    `паттерны`,
+    `молотки`,
+    `желания`,
+    `идеологии`,
+    `идеи`,
+    `ограничения`,
+    `решения`,
+    `продукты`,
+    `инструменты`,
+    `средства`,
+    `цели`,
+]
+
+const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1)
+
+const getAdvice = () => {
+    const action = actions[Math.floor(Math.random() * actions.length)]
+    const subject = subjects[Math.floor(Math.random() * subjects.length)]
+
+    const capitalizedAction = capitalizeFirstLetter(action)
+
+    return `${capitalizedAction} ${subject}.`
+}
+
+document.getElementById('quote').innerHTML = getAdvice()
+
+document.getElementById('new').onclick = () => {
+    document.getElementById('quote').innerHTML = getAdvice()
+}
+
